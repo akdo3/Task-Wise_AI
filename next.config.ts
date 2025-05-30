70f0c8d5b957ac4f7b1821c55aa3e8ff65659af8
akdo <109563467+akdo3@users.noreply.github.com>
2025-05-30 03:43:17 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -20,4 +20,19 @@ const nextConfig: NextConfig = {
   },
 };
 
+const repoName = 'Task-Wise_AI';
+const isProd = process.env.NODE_ENV === 'production';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  images: {
+     unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
+
 export default nextConfig;
