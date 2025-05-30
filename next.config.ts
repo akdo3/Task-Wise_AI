86124d3589bd4df79e9922b2d8dc2e3c4dd15c5b
akdo <109563467+akdo3@users.noreply.github.com>
2025-05-30 03:53:42 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,3 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-};
-
 const repoName = 'Task-Wise_AI';
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -34,5 +12,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-export default nextConfig;
